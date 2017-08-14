834eba3af91b979d878f1d22a1b4c0dbef79babc
arider <arider@stanford.edu>
2017-08-14 10:44:32 -0700

    in the progress of making module to find cantilever angle and origin from beam profile data

--- scripts/beam_utils.py
+++ scripts/beam_utils.py
@@ -9,34 +9,7 @@ import scipy.signal as sig
 import scipy
 import glob
 from scipy.optimize import curve_fit
-
-
-data_dir1 = r"C:\Data\20170704\profiling\ysweep5"
-data_dir2 = r"C:\Data\20170704\profiling\zsweep5"
-out_dir = r"C:\Data\20170704\profiling\output"
-#data_dir2 = r"C:\Data\20160429\beam_profiles1"
-
-multi_dir = True
-height_to_plot = 0.
-
-log_profs = True
-
-ROI = [-80, 80] # um
-#OFFSET = 2.*10**(-5)
-OFFSET = 0
-
-msq_fit = False
-gauss_fit = True
-
-#stage x = col 17, stage y = 18, stage z = 19
-stage_column = 19
-stage_column2 = 18
-
-data_column = 4
-data_column2 = 0  # For data circa 2016
-
-cant_cal = 8. #um/volt
-
+import bead_util as bu
 
 def spatial_bin(xvec, yvec, bin_size = .13):
     fac = 1./bin_size
@@ -51,32 +24,19 @@ def spatial_bin(xvec, yvec, bin_size = .13):
         y_errors[i] = scipy.stats.sem(yvec[idx])
     return bins, y_binned, y_errors
     
-        
-    
 
-def profile(fname, ends = 100, stage_cal = 8.):
-    dat, attribs, f = bu.getdata(fname)
-    dat = dat[ends:-ends, :]
-    #plt.plot(dat[:, data_column])
-    #plt.show()
-    if 'zsweep' in fname:
-        stage_column = 19
-    elif 'ysweep' in fname:
+class Profile:
+    'class representing a beam profile'
+       
+    def __init__(self, fname, stage_column, ends = 100, stage_cal = 8.):
+        dat, attribs, f = bu.getdata(fname)
+        dat = dat[ends:-ends, :]
         stage_column = 18
-    dat[:,stage_column]*=stage_cal
-    h = attribs["stage_settings"][0]*cant_cal
+        dat[:,stage_column]*=stage_cal
+        h = attribs["stage_settings"][2]*cant_cal
     f.close()
     b, a = sig.butter(1, 1)
-    if '2016' in fname:
-        int_filt = sig.filtfilt(b, a, dat[:, data_column2])
-    else:
-        int_filt = sig.filtfilt(b, a, dat[:, data_column])
-    #plt.plot(int_filt)
-    #fft = np.fft.rfft(int_filt)
-    #freqs = np.fft.rfftfreq(len(int_filt), d=1./5000)
-    #plt.figure()
-    #plt.loglog(freqs, fft * fft.conj())
-    #plt.show()
+    int_filt = sig.filtfilt(b, a, dat[:, data_column])
     proft = np.gradient(int_filt)- OFFSET
     if 'zsweep' in fname:
         stage_filt = sig.filtfilt(b, a, dat[:, 19])
