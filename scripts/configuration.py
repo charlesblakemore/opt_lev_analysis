7b83c0df69b0d2fc0a90faeedd82f65e78bd5f81
arider <arider@stanford.edu>
2017-09-21 10:28:59 -0700

    started a configuration file that contains the parameters for the analysis

---
+++ scripts/configuration.py
@@ -0,0 +1,15 @@
+#################################################################################Configuration file specifying all of the parameters used in the analysis
+###############################################################################
+import numpy
+
+####Physical parameters
+bead_radius = 2.43e-6 #m
+bead_rho = 2.2e3 #kg/m^3
+kb = 1.3806488e-23 #J/K
+bead_mass = (4./3.)*(np.pi*bead_radius**3)*bead_rho
+e_charge = 1.60217662e-19 #electron charge in C
+nucleon_mass = 1.6749e-27 #proton mass in kg
+
+
+ 
+
