850139fb1d0a9bd6e6543e5c4eacc712f139e16f
arider <arider@stanford.edu>
2018-06-26 14:15:13 -0700

    started a script to figure out grav_util

---
+++ scripts/alex_tests/alex_grav_util_test.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import bead_util as bu
+import grav_util as gu
+
+
+path = "/data/20180625/bead1/grav_data/no_shield/X60-80um_Z15-25um_17Hz"
+files = bu.find_all_fnames(path) 
