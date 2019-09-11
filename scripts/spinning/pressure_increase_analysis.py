8ba08c5c9cd3ec6bb2921a036ce24fbae0cc82dc
denzal <domartin@stanfor.edu>
2019-09-11 08:58:21 -0700

    Added code to extract correlation of rotation signal with drive signal

--- scripts/spinning/pressure_increase_analysis.py
+++ scripts/spinning/pressure_increase_analysis.py
@@ -6,7 +6,7 @@ import scipy.signal as ss
 from scipy.optimize import curve_fit
 import re, sys
 
-save = True
+save = True 
 
 #path = "/data/20181204/bead1/high_speed_digitizer/pramp/50k_zhat_8vpp_3"
 
@@ -18,10 +18,10 @@ save = True
 #path_list = ['/daq2/20190626/bead1/spinning/pramp/N2/50kHz_4Vpp',\
 #		'/daq2/20190626/bead1/spinning/pramp/N2/50kHz_4Vpp_2',\
 #		'/daq2/20190626/bead1/spinning/pramp/N2/50kHz_4Vpp_3']	
-path_list = ['/daq2/20190905/bead1/spinning/pramp/He/50kHz_4Vpp_4',\
-			 '/daq2/20190905/bead1/spinning/pramp/He/50kHz_4Vpp_5']
-drive_ax = 0
-data_ax = 1
+path_list = ['/daq2/20190905/bead1/spinning/pramp/He/50kHz_4Vpp_4']#,\
+			 #'/daq2/20190905/bead1/spinning/pramp/He/50kHz_4Vpp_5']
+drive_ax = 1
+data_ax = 0
 
 #out_f = "/processed_data/spinning/pramp_data/49k_200vpp"
 #out_f = "/home/dmartin/analyzedData/20190430/20190430_1vpp_50kHz_0"
@@ -30,20 +30,20 @@ data_ax = 1
 #out_f = "/home/dmartin/analyzedData/20190514/pramp/He/50kHz_4Vpp_3"
 out_f = "/home/dmartin/analyzedData/20190905/pramp/He/"
 
-plot_dat = False
+plot_dat = False 
  
 def line(x, m, b):
     return m*x + b
 
 def dec2(arr, fac):
     return ss.decimate(ss.decimate(arr, fac), fac)
-
+corr = []
 for i, path in enumerate(path_list):
 	bu.make_all_pardirs(out_f)
 	
 	files, zero = bu.find_all_fnames(path, sort_time=True)
 	
-	files = files[0:400]
+	files = files[0:500]
 	
 	fi_init = 50000
 	init_file = 0
@@ -70,29 +70,34 @@ for i, path in enumerate(path_list):
 	bfreq2 = np.abs(freqs-fc/2.)>bw/2.
 	
 	for i, f in enumerate(files[init_file:final_file]):
-	    bu.progress_bar(i, n_file)
-	    sys.stdout.flush()
-	    #print f
-	    try:
-	       
-	        obj = hsDat(f)
-	        fft = np.fft.rfft(obj.dat[:, data_ax])
-	        fft2 = np.fft.rfft(obj.dat[:, drive_ax])
-	        fft[bfreq] = 0.
-	        fft2[bfreq2] = 0.
-	        if plot_dat:
-	            plt.loglog(freqs, np.abs(fft))
-	            plt.loglog(freqs, np.abs(fft2))
-	            plt.show()
-	        phases[i] = np.angle(np.sum(fft))
-	        dphases[i] = np.angle(np.sum(fft2))
-	        pressures[i, :] = obj.attribs["pressures"]
-	        times[i] = obj.attribs['time']
-	    except:
-	        print "bad file"
+		bu.progress_bar(i, n_file)
+		sys.stdout.flush()
+		#print f
+		try:
+		   
+			obj = hsDat(f)
+			fft = np.fft.rfft(obj.dat[:, data_ax])
+			fft2 = np.fft.rfft(obj.dat[:, drive_ax])
+			fft[bfreq] = 0.
+			fft2[bfreq2] = 0.
+			if plot_dat:
+				plt.loglog(freqs, np.abs(fft))
+				plt.loglog(freqs, np.abs(fft2))
+				plt.show()
+			phases[i] = np.angle(np.sum(fft))
+			dphases[i] = np.angle(np.sum(fft2))
+			pressures[i, :] = obj.attribs["pressures"]
+			times[i] = obj.attribs['time']
+		except:
+			print "bad file"
+		
+		amp = np.abs(fft[freqs == 100e3])
+		damp = np.abs(fft2[freqs == 50e3])
+		
+		corr.append(amp/damp)
 	
 	dphases[dphases<0.]+=np.pi
-	
+	corr = np.array(corr)
 	
 	phi = phases - 2.*dphases
 	phi[phi>np.pi]-=2.*np.pi
@@ -100,6 +105,7 @@ for i, path in enumerate(path_list):
 	
 	meas_name = path.split('/')[-1]	
 	if save:
-	    np.save(out_f +  meas_name + '_phi.npy', phi)
-	    np.save(out_f +  meas_name + "_pressures.npy", pressures)
-	    np.save(out_f +  meas_name + "_time.npy", times)
+		np.save(out_f +  meas_name + '_phi.npy', phi)
+		np.save(out_f +  meas_name + "_pressures.npy", pressures)
+		np.save(out_f +  meas_name + "_time.npy", times)
+		np.save(out_f +  meas_name + "_corr.npy", corr)	
