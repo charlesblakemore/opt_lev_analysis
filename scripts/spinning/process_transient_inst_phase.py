c1db6fdfa0a7b22f7d9a04f2c0259ec7f461209c
Denzal <dom4822@yahoo.com>
2020-02-16 17:08:51 -0800

    Added new functions to process_transient...py 	-Gaussian check to check if y data points are Gaussian distributed 	-Extract y err to extract the standard deviation of the y data points with the corresponding start indices and y curves 	used so that the fit transients function uses the same curves and start for fitting.

--- scripts/spinning/process_transient_inst_phase.py
+++ scripts/spinning/process_transient_inst_phase.py
@@ -11,7 +11,7 @@ from scipy.optimize import curve_fit
 from memory_profiler import profile
 from memory_profiler import memory_usage
 from plot_phase_vs_pressure_many_gases import build_full_pressure
-
+from iminuit import Minuit
 
 import gc
 
@@ -33,22 +33,34 @@ folders = ['/data/old_trap/20200130/bead1/spinning/series_3/base_press/change_ph
 
 base_folder = '/data/old_trap/20200130/bead1/spinning/series_4/base_press/change_phi_offset_3/'
 
+save_base_name = '/home/dmartin/Desktop/analyzedData/20200130/images/hist/'
+
 files, zeros, folders = bu.find_all_fnames(base_folder, ext='.h5', sort_time=True, \
                     add_folders=True)
-print(folders)
-raw_input()
 
-#folders = ['/data/old_trap/20200130/bead1/spinning/series_2/base_press/change_phi_offset_0_85_dg/']
-
-#folders = ['/data/old_trap/20200130/bead1/spinning/series_3/base_press/change_phi_offset/0_dg_noise_added_0_009/']
-
-save = False
+save = False 
 multiple_folders = True
 
+#gaussian check params:
+save_histograms = True
+num_save_hist = 10
+num_y_data_points = 500000
+num_bins = 10
+max_length_from_end = 150000
+
+#fit transients params:
+dist_to_end = 10
+a_fix = True
+b_fix = True
+c_fix = False
+d_fix = True
+start_c = -2
+migrad_ncall = 100000
+
 fit_curves = False
-plot = True
+plot = False
 plot_multiple = False
-plot_drive_and_inst_phase = True
+plot_drive_and_inst_phase = False
 
 if save:
     save_base_name = '/home/dmartin/Desktop/analyzedData/20200130/spinning/series_3/base_press/'
@@ -64,11 +76,8 @@ libration_freq = 373
 
 threshold = 0.6
 
-def exp(x, a, b , c):
-    return a*np.exp((x-b)*c)  
-
-def chi_sq_single(y, f, yvar):
-    return ((y-f)/yvar)**2
+def exp(x, a, b , c, d):
+    return a*np.exp((x-b)*c) + d 
 
 def plot_transient(filenames, lf, bw, thr, pl, fit):
 
@@ -228,6 +237,316 @@ def plot_transient(filenames, lf, bw, thr, pl, fit):
         
     return fit_params, fit_pcov, phi_inst_amps_x, phi_inst_amps_y, dg
 
+def extract_y_err(filenames, lf, bw, thr, dte):
+
+    y_curves = []
+    start_inds = []
+    for i, f in enumerate(filenames):
+        print(f, i)
+        obj = hsDat(f)
+        
+        dg = obj.attribs['start_pm_dg']
+        
+        Ns = obj.attribs['nsamp']
+        Fs = obj.attribs['fsamp']
+        freqs = np.fft.rfftfreq(Ns, 1./Fs)
+
+        tarr = np.arange(Ns)/Fs
+
+        crossp = obj.dat[:,crossp_ind]
+
+        crossp_fft = np.fft.rfft(crossp)
+
+        crossp_z = signal.hilbert(crossp)
+        
+        crossp_phase = signal.detrend(np.unwrap(np.angle(crossp_z)))
+       
+        crossp_phase_unfilt = np.fft.rfft(crossp_phase)
+        
+
+        crossp_phase = bp_filt(crossp_phase, lf, Ns, Fs, bw)
+
+        crossp_phase_z = signal.hilbert(crossp_phase)
+        crossp_phase_amp = np.abs(crossp_phase_z)
+
+        window = np.hanning(Ns)
+        crossp_phase_fft = np.fft.rfft(crossp_phase*window)
+
+        diff_max=0
+        for j, val in enumerate(drive_phase):
+            diff = np.abs(drive_phase[0] - val)
+            
+            if diff > diff_max:
+                diff_max = diff
+            if diff_max > thr:
+                start = val
+                start_ind = j
+                print('change found')
+                break
+
+        max_val = np.amax(crossp_phase_amp)
+        max_ind = np.argmax(crossp_phase_amp)
+        
+        print(max_val)
+
+        start_ind = max_ind
+        
+        mask = (tarr > tarr[start_ind])
+        
+        end_mask = (tarr > Ns/Fs - dte)
+
+        #plt.plot(tarr[end_mask], crossp_phase_amp[end_mask])
+        #plt.show()
+
+        #plt.plot(tarr[mask],crossp_phase[mask])
+        #plt.plot(tarr[mask], crossp_phase_amp[mask])
+        #plt.scatter(tarr[start_ind], max_val)
+        #plt.show()
+        
+        x = tarr[mask]
+        y = crossp_phase_amp[mask]
+        
+
+        start_inds.append(start_ind)
+        y_curves.append(y)
+
+    y_err = np.std(y_curves, axis=0)
+    print(y_err)
+    raw_input()
+
+    return y_err, start_inds, y_curves
+
+def fit_transients(filenames, lf, bw, thr, dte, fix_a, fix_b, fix_c, fix_d, start_c, migrad_ncall):
+
+    obj0 = hsDat(filenames[0])
+
+    t0 = obj0.attribs['time']
+
+    colors = bu.get_color_map(len(filenames), 'inferno')
+
+    phi_inst_amps_x = []#[0 for i in range(len(filenames))]
+    phi_inst_amps_y = []#[0 for i in range(len(filenames))]
+    fit_curves_x= []#[0 for i in range(len(filenames))]
+    fit_curves_y= []#[0 for i in range(len(filenames))]
+    fit_params = []#[0 for i in range(len(filenames))]
+    fit_pcov = []#[0 for i in range(len(filenames))]
+
+    def chi_sq(a, b ,c, d):
+        return np.sum(((y-exp(x, a, b, c, d))/y_err)**2.)
+
+    y_err, start_inds, y_curves = extract_y_err(filenames, lf, bw, thr, pl, fit, dte)
+
+    for i, f in enumerate(filenames):
+        print(f, i)
+        obj = hsDat(f)
+        
+        dg = obj.attribs['start_pm_dg']
+        
+        Ns = obj.attribs['nsamp']
+        Fs = obj.attribs['fsamp']
+        freqs = np.fft.rfftfreq(Ns, 1./Fs)
+
+        tarr = np.arange(Ns)/Fs
+
+        crossp = obj.dat[:,crossp_ind]
+        drive = obj.dat[:,drive_ind]
+
+        drive_fft = np.fft.rfft(drive)
+        crossp_fft = np.fft.rfft(crossp)
+
+        #plt.loglog(freqs, np.abs(crossp_fft))
+        #plt.loglog(freqs, np.abs(drive_fft))
+        #plt.show()
+
+        crossp_z = signal.hilbert(crossp)
+        drive_z = signal.hilbert(drive)
+        
+
+        drive_phase = signal.detrend(np.unwrap(np.angle(drive_z)))
+        crossp_phase = signal.detrend(np.unwrap(np.angle(crossp_z)))
+       
+        crossp_phase_unfilt = np.fft.rfft(crossp_phase)
+        
+
+        crossp_phase = bp_filt(crossp_phase, lf, Ns, Fs, bw)
+        #crossp_phase = hp_filt(crossp_phase, lf, Ns, Fs)
+
+        crossp_phase_z = signal.hilbert(crossp_phase)
+        crossp_phase_amp = np.abs(crossp_phase_z)
+
+        window = np.hanning(Ns)
+        crossp_phase_fft = np.fft.rfft(crossp_phase*window)
+
+        crossp_phase_psd, freqs = mlab.psd(crossp_phase, Ns, Fs, window=mlab.window_none(crossp_phase))#mlab.window_hanning(crossp_phase))
+        drive_phase_psd, freqs = mlab.psd(drive_phase, Ns, Fs, window=mlab.window_none(drive_phase))#(drive_phase))
+
+        #plt.loglog(freqs, np.abs(crossp_phase_fft))
+        #plt.show()
+
+        #plt.plot(tarr, crossp_phase)
+        #plt.plot(tarr, drive_phase)
+        #plt.show()
+
+        diff_max=0
+        for j, val in enumerate(drive_phase):
+            diff = np.abs(drive_phase[0] - val)
+            
+            if diff > diff_max:
+                diff_max = diff
+            if diff_max > thr:
+                start = val
+                start_ind = j
+                print('change found')
+                break
+
+        max_val = np.amax(crossp_phase_amp)
+        max_ind = np.argmax(crossp_phase_amp)
+        
+        print(max_val)
+
+        start_ind = max_ind
+        
+        mask = (tarr > tarr[start_ind])
+        
+        end_mask = (tarr > Ns/Fs - dte)
+
+        #plt.plot(tarr[end_mask], crossp_phase_amp[end_mask])
+        #plt.show()
+
+        d_guess = np.mean(crossp_phase_amp[end_mask])
+        
+        print(d_guess)
+
+        #plt.plot(tarr[mask],crossp_phase[mask])
+        #plt.plot(tarr,drive_phase)
+        #plt.plot(tarr[mask], crossp_phase_amp[mask])
+        #plt.scatter(tarr[start_ind], max_val)
+        #plt.show()
+        
+        x = tarr[mask]
+        y = crossp_phase_amp[mask]
+        y_err = np.ones(len(y)) 
+        
+        m=Minuit(chi_sq, a=max_val, fix_a=fix_a, error_a=0.1, b=tarr[max_ind], fix_b=fix_b, \
+                c=start_c, fix_c=fix_c, limit_c=[-100000,0],  error_c=0.1, d=d_guess,fix_d=fix_d, print_level=1) 
+         
+        m.migrad(ncall=migrad_ncall)
+
+        print('done', m.values)
+
+        #x = np.linspace(tarr[start_ind:][0], tarr[start_ind:][-1], len(tarr[start_ind:])*10)
+
+        p = [m.values['a'], m.values['b'], m.values['c'], m.values['d']]
+
+        #plt.plot(tarr[mask],crossp_phase_amp[mask])
+        #plt.plot(x, exp(x, *p))
+        #plt.show()
+        
+        print(chi_sq(*p))
+
+        #if fit:
+        #                
+        #    mask = (tarr > tarr[start_ind]) & (tarr < end )
+
+        #    print(popt)
+
+        #    fit_label = r'a$e^{c(t-b)}$, ' + 'a={} rad, b={} s, c={} Hz'.format(popt[0].round(2), popt[1].round(2), popt[2].round(2))
+
+        #    fit_curve = exp(x, *popt)
+        #    fit_x = x
+
+        #    fit_params.append(popt)
+        #    fit_pcov.append(pcov)
+        #    fit_curves_x.append(fit_x)
+        #    fit_curves_y.append(fit_curve)
+        #    phi_inst_amps_x.append(tarr[start_ind:])
+        #    phi_inst_amps_y.append(crossp_phase_amp[start_ind:])
+
+    return fit_params, fit_pcov, phi_inst_amps_x, phi_inst_amps_y, dg
+
+def gaussian_check(filenames, lf, bw, thr, pl, max_lfe, nbins, n, nsave, save_hists):
+    
+    print('gaussian_check')
+
+    y_arr = [0 for i in range(len(filenames))]
+
+    for i, f in enumerate(filenames):
+        print(f, i)
+        obj = hsDat(f)
+
+        dg = obj.attribs['current_pm_dg']
+
+        Ns = obj.attribs['nsamp']
+        Fs = obj.attribs['fsamp']
+        tarr = np.arange(Ns)/Fs
+
+        crossp = obj.dat[:,crossp_ind]
+        
+        crossp_z = signal.hilbert(crossp)
+
+        crossp_phase = signal.detrend(np.unwrap(np.angle(crossp_z)))
+
+
+        crossp_phase = bp_filt(crossp_phase, lf, Ns, Fs, bw)
+        
+        crossp_phase_z = signal.hilbert(crossp_phase)
+        crossp_phase_amp = np.abs(crossp_phase_z)
+        
+        max_val = np.amax(crossp_phase_amp)
+        max_ind = np.argmax(crossp_phase_amp)
+
+        start_ind = max_ind
+
+        
+        mask = (tarr > tarr[start_ind])
+        
+        length_of_arrays = len(tarr) - max_lfe #length of all arrays after cutting out data points greater than this value
+        
+        if len(crossp_phase_amp[mask]) < length_of_arrays: #Check if array is too short for proper binning
+            print('array is too short')
+            
+        else:
+            crossp_phase_amp = crossp_phase_amp[mask][:length_of_arrays] 
+            print('cut')
+        
+        #plt.plot(crossp_phase_amp)
+        
+        print(len(crossp_phase_amp))
+        
+        y_arr[i] = crossp_phase_amp
+    
+    y_arr = np.array(y_arr)
+    hist_arr = []#[0 for i in range(n)]#len(crossp_phase_amp))]
+    bins_arr = []#[0 for i in range(n)]
+    for j in range(n):#len(crossp_phase_amp)):
+        if j%(len(crossp_phase_amp)/10000):
+            col = j
+            hist, bins = np.histogram(y_arr[:,col], bins=nbins)
+            hist_arr.append(hist)
+            bins_arr.append(bins)
+    
+    hist_arr = np.array(hist_arr)
+    bins_arr = np.array(bins_arr)
+   
+    print(bins_arr)
+    for i in range(nsave):
+        meas_name = f.split('/')[-2]     
+        
+        save_name = save_base_name +  meas_name + '_{}_dg_{}'.format(dg, i) + '.png'
+        
+        hist = hist_arr[i]
+        bins = bins_arr[i][:-1]
+        width = np.abs(bins_arr[i][1]-bins_arr[i][0])
+        
+        if save_hists:
+            plt.bar(bins, hist, width=width)
+            plt.ylabel('Counts')
+            plt.xlabel(r'y_{}'.format(i))
+            plt.savefig(save_name)
+            plt.close()
+            #add way to save the histograms to be fitted by gaussians later
+    
+
 def plot_multiple(files, lf, bw):
     for i, f in enumerate(files):
 
@@ -295,20 +614,6 @@ def plot_drive_w_phase(f, lf, bw):
     plt.legend()
     plt.show()
 
-if plot_drive_and_inst_phase:
-    files, zero = bu.find_all_fnames(folders[1], sort_time=True)
-    
-    for i,f in enumerate(files):
-        plot_drive_w_phase(f, libration_freq, wind_bandwidth)
-
-if False:#plot_multiple:
-    file_arr = []
-    for i, folder in enumerate(folders):
-        files, zero = bu.find_all_fnames(folder, sort_time=True)
-
-        file_arr.append(files[-1])
-        
-    plot_multiple(file_arr, libration_freq, wind_bandwidth)
 
 if multiple_folders:
     
@@ -318,42 +623,28 @@ if multiple_folders:
     measured_a_avg = []
     measured_a_std = []
     dg_arr = []
-    for i, folder in enumerate(folders):
+    for i, folder in enumerate(folders[1:]):
         files, zero = bu.find_all_fnames(folder, sort_time=True)
         files = files[1:]
-        fit_params, fit_pcov, phi_inst_amps_x, phi_inst_amps_y, dg = \
-                plot_transient(files, libration_freq, wind_bandwidth, threshold, plot, fit_curves)
         
-        #fit_params = np.array(fit_params)
-
-        #print(fit_params)
-        #avg = np.mean(fit_params, axis=0)
-        #std = np.std(fit_params, axis=0)
-        #print(avg)
-        #measured_a_avg.append(avg[0])
-        #measured_a_std.append(std[0])
-        #measured_t_offset = avg[1], std[1]
-        #measured_damp_avg.append(avg[2])
-        #measured_damp_std.append(std[2])
-        #dg_arr.append(dg)
-    
-        
-
-        
-        if save:
-            meas_name = files[-1].split('/')[-2]
+        #gaussian_check(files[1:], libration_freq, wind_bandwidth, threshold, plot, max_length_from_end, num_bins, num_y_data_points, num_save_hist, save_histograms)
+        fit_transients(files, libration_freq, wind_bandwidth, threshold, plot, fit_curves, dist_to_end, \
+                a_fix, b_fix, c_fix, d_fix,  start_c, migrad_ncall)
+        ##fit_params, fit_pcov, phi_inst_amps_x, phi_inst_amps_y, dg = \
+        ##        plot_transient(files, libration_freq, wind_bandwidth, threshold, plot, fit_curves)
+        #
+        #if save:
+        #    meas_name = files[-1].split('/')[-2]
+        #    
+        #    
+        #    if meas_name == '':
+        #        print('empty name')
+        #        meas_name = '{}'.format(i)
+        #    
+        #    save_name = save_base_name + meas_name
+        #    
+        #    np.savez(save_name, fit_params=fit_params, fit_pcov=fit_pcov, phi_inst_amps_x=phi_inst_amps_x, phi_inst_amps_y=phi_inst_amps_y, dg=dg)
             
-            
-            if meas_name == '':
-                print('empty name')
-                meas_name = '{}'.format(i)
-            
-            save_name = save_base_name + meas_name
-            
-            np.savez(save_name, fit_params=fit_params, fit_pcov=fit_pcov, phi_inst_amps_x=phi_inst_amps_x, phi_inst_amps_y=phi_inst_amps_y, dg=dg)
-            
-    #plt.plot(dg_arr, measured_damp_avg)
-    #plt.show()
 else:
     fit_params, fit_curves, phi_inst_amps = plot_transient(files[1:], libration_freq, wind_bandwidth, threshold)
 
@@ -368,6 +659,20 @@ else:
             save_name = save_base_name + meas_name
             np.savez(save_name, fit_params=fit_params, fit_curves=fit_curves, phi_inst_amps=phi_inst_amps)
 
+#if plot_drive_and_inst_phase:
+#    files, zero = bu.find_all_fnames(folders[1], sort_time=True)
+#    
+#    for i,f in enumerate(files):
+#        plot_drive_w_phase(f, libration_freq, wind_bandwidth)
+#
+#if plot_multiple:
+#    file_arr = []
+#    for i, folder in enumerate(folders):
+#        files, zero = bu.find_all_fnames(folder, sort_time=True)
+#
+#        file_arr.append(files[-1])
+#        
+#    plot_multiple(file_arr, libration_freq, wind_bandwidth)
 
 
 
