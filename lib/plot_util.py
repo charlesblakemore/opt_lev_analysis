40dc9b911764c786e372af8deb14105ce6b9ffd8
Charles Blakemore <chas.blakemore@gmail.com>
2022-03-24 17:58:23 -0700

    Added a function to apply labels tangent to a curve. Based on a stackoverflow solution, but with some modifications.

--- lib/plot_util.py
+++ lib/plot_util.py
@@ -1,4 +1,4 @@
-import sys, os, re
+import sys, os, re, math
 
 import numpy as np
 
@@ -9,7 +9,6 @@ import matplotlib.colors as colors
 from matplotlib import cm
 from matplotlib.ticker import Locator
 from mpl_toolkits.axes_grid1 import make_axes_locatable
-
 
 
 def get_single_color(val, cmap='plasma', vmin=0.0, vmax=1.0, log=False):
@@ -172,6 +171,67 @@ def add_colorbar(fig, ax, size=0.1, pad=0.05, vmin=0.0, vmax=1.0, \
     return ax_cb, cb
 
 
+
+
+
+#Label line with line2D label data
+def labelLine(line, x, x_offset=0.0, y_offset=0.0, \
+              alpha=0.0, label=None, align=True, **kwargs):
+
+    ax = line.axes
+    xdata = line.get_xdata()
+    ydata = line.get_ydata()
+
+    if (x < xdata[0]) or (x > xdata[-1]):
+        print('x label location is outside data range!')
+        return
+
+    #Find corresponding y co-ordinate and angle of the line
+    ip = 1
+    for i in range(len(xdata)):
+        if x < xdata[i]:
+            ip = i
+            break
+
+    y = ydata[ip-1] + (ydata[ip]-ydata[ip-1])*(x-xdata[ip-1])/(xdata[ip]-xdata[ip-1])
+
+    if not label:
+        label = line.get_label()
+
+    if align:
+        #Compute the slope
+        dx = xdata[ip] - xdata[ip-1]
+        dy = ydata[ip] - ydata[ip-1]
+        ang = math.degrees(math.atan2(dy,dx))
+
+        #Transform to screen co-ordinates
+        pt = np.array([x,y]).reshape((1,2))
+        trans_angle = ax.transData.transform_angles(np.array((ang,)),pt)[0]
+
+    else:
+        trans_angle = 0
+
+    #Set a bunch of keyword arguments
+    if 'color' not in kwargs:
+        kwargs['color'] = line.get_color()
+
+    if ('horizontalalignment' not in kwargs) and ('ha' not in kwargs):
+        kwargs['ha'] = 'center'
+
+    if ('verticalalignment' not in kwargs) and ('va' not in kwargs):
+        kwargs['va'] = 'center'
+
+    if 'backgroundcolor' not in kwargs:
+        kwargs['backgroundcolor'] = ax.get_facecolor()
+
+    if 'clip_on' not in kwargs:
+        kwargs['clip_on'] = True
+
+    if 'zorder' not in kwargs:
+        kwargs['zorder'] = 2.5
+        
+    t = ax.text(x+x_offset, y+y_offset, label, rotation=trans_angle, **kwargs)
+    t.set_bbox(dict(alpha=alpha))
 
 
 
